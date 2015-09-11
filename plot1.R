65667e68a1fa991c7088439d4ce7944f80c13acd
Saran Shanmugam <saran.shanmugam@gmail.com>
2015-09-11 17:33:05 -0500

    First Commit

---
+++ plot1.R
@@ -0,0 +1,14 @@
+## read the downloaded dataset
+setwd("/Users/saran/explore")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl,destfile="household_power_consumption.zip",method="curl")
+unzip(zipfile="./household_power_consumption.zip")
+data <- read.table("./household_power_consumption.txt", header=TRUE, sep=";",stringsAsFactors=FALSE, dec=".")
+## select just the 2 dates data
+ twodaydata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+
+Globalactivepower <- as.numeric(twodaydata$Global_active_power)
+##write the graph into the file
+png("plot1.png")
+hist(Globalactivepower,main="Global Active Power",col="Red",xlab="Global Active Power (Kilowatts)",ylab="Frequency")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,28 @@
+## read the downloaded dataset
+setwd("/Users/saran/explore")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl,destfile="household_power_consumption.zip",method="curl")
+unzip(zipfile="./household_power_consumption.zip")
+data <- read.table("./household_power_consumption.txt", header=TRUE, sep=";",stringsAsFactors=FALSE, dec=".")
+## select just the 2 dates data
+twodaydata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+Globalactivepower <- as.numeric(twodaydata$Global_active_power)
+Globalreactivepower <- as.numeric(twodaydata$Global_reactive_power)
+Voltage <- as.numeric(twodaydata$Voltage)
+Submetering1 <- as.numeric(twodaydata$Sub_metering_1)
+Submetering2 <- as.numeric(twodaydata$Sub_metering_2)
+Submetering3 <- as.numeric(twodaydata$Sub_metering_3)
+datetime <- strptime(paste(twodaydata$Date,twodaydata$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+##write the graph into the file
+png("plot4.png")
+par(mfrow= c(2,2))
+plot(datetime,Globalactivepower,type="l",xlab="",ylab="Global Active Power")
+plot(datetime,Voltage,type="l")
+plot(datetime,Submetering1,type="l",xlab="",ylab="Energy sub metering")
+lines(datetime,Submetering2,type="l",col="red")
+lines(datetime,Submetering3,type="l",col="blue")
+legend("topright",border="black",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=3,
+       col=c("black","red","blue"))
+plot(datetime,Globalreactivepower,type="l",ylab="Global_reactive_power")
+
+dev.off()
